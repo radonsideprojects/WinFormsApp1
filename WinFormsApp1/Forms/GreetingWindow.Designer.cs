d6f145ef9ab1d4adf44bd9a401568b4f3746c9ca
radonsideprojects <artemkaczycnhy@gmail.com>
2024-10-31 12:36:20 +0200

    Made the main form

--- WinFormsApp1/Forms/GreetingWindow.Designer.cs
+++ WinFormsApp1/Forms/GreetingWindow.Designer.cs
@@ -28,20 +28,96 @@
         /// </summary>
         private void InitializeComponent()
         {
+            textBox1 = new TextBox();
+            label1 = new Label();
+            groupBox1 = new GroupBox();
+            textBox2 = new TextBox();
+            linkLabel1 = new LinkLabel();
+            button1 = new Button();
+            groupBox1.SuspendLayout();
             SuspendLayout();
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(105, 62);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(136, 23);
+            textBox1.TabIndex = 0;
+            textBox1.Text = "GoofyGoober";
+            // 
+            // label1
+            // 
+            label1.Font = new Font("Segoe UI", 24F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label1.Location = new Point(12, 9);
+            label1.Name = "label1";
+            label1.Size = new Size(350, 59);
+            label1.TabIndex = 1;
+            label1.Text = "Title";
+            label1.TextAlign = ContentAlignment.MiddleCenter;
+            // 
+            // groupBox1
+            // 
+            groupBox1.Controls.Add(button1);
+            groupBox1.Controls.Add(linkLabel1);
+            groupBox1.Controls.Add(textBox2);
+            groupBox1.Controls.Add(textBox1);
+            groupBox1.Location = new Point(12, 71);
+            groupBox1.Name = "groupBox1";
+            groupBox1.Size = new Size(350, 204);
+            groupBox1.TabIndex = 2;
+            groupBox1.TabStop = false;
+            groupBox1.Text = "Login";
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(105, 91);
+            textBox2.Name = "textBox2";
+            textBox2.PasswordChar = '*';
+            textBox2.Size = new Size(136, 23);
+            textBox2.TabIndex = 1;
+            // 
+            // linkLabel1
+            // 
+            linkLabel1.AutoSize = true;
+            linkLabel1.Location = new Point(150, 146);
+            linkLabel1.Name = "linkLabel1";
+            linkLabel1.Size = new Size(49, 15);
+            linkLabel1.TabIndex = 2;
+            linkLabel1.TabStop = true;
+            linkLabel1.Text = "Register";
+            // 
+            // button1
+            // 
+            button1.Location = new Point(105, 120);
+            button1.Name = "button1";
+            button1.Size = new Size(136, 23);
+            button1.TabIndex = 3;
+            button1.Text = "Login";
+            button1.UseVisualStyleBackColor = true;
             // 
             // GreetingWindow
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(521, 414);
+            ClientSize = new Size(374, 287);
+            Controls.Add(groupBox1);
+            Controls.Add(label1);
             FormBorderStyle = FormBorderStyle.FixedSingle;
             MaximizeBox = false;
             Name = "GreetingWindow";
-            Text = "GreetingWindow";
+            Text = "Title";
+            groupBox1.ResumeLayout(false);
+            groupBox1.PerformLayout();
             ResumeLayout(false);
         }
 
         #endregion
+
+        private TextBox textBox1;
+        private Label label1;
+        private GroupBox groupBox1;
+        private TextBox textBox2;
+        private Button button1;
+        private LinkLabel linkLabel1;
     }
 }
